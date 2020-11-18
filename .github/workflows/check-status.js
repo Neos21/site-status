329a6f66260b65404518d40d4b3768b6ae1b92bc
Neos21 <neos21@gmail.com>
2020-11-18 21:29:18 +0900

    Modify

--- .github/workflows/check-status.js
+++ .github/workflows/check-status.js
@@ -217,18 +217,9 @@ function createReadmeText(todayString, infos) {
     + `
 
 
-## Author
-
-[Neo](https://neos21.net/)
-
-
 ## Links
 
 - [Neo's World](https://neos21.net/)
-- [Corredor](https://neos21.hatenablog.com/)
-- [Murga](https://neos21.hatenablog.jp/)
-- [El Mylar](https://neos21.hateblo.jp/)
-- [Neo's GitHub Pages](https://neos21.github.io/)
 - [GitHub - Neos21](https://github.com/Neos21/)
 `;
   
