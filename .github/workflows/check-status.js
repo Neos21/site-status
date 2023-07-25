78d4d804f550d98de808d7acffeb0679cbbd2743
Neos21 <16625731+Neos21@users.noreply.github.com>
2023-07-25 15:01:28 +0900

    Update check-status.js

--- .github/workflows/check-status.js
+++ .github/workflows/check-status.js
@@ -8,10 +8,10 @@ const https = require('https');
 
 /** サイト定義 */
 const sites = [
-  { name: "Neo's World", https: 'neos21.net'   , http: 'neos21.github.io/neos21.net', statusJsonPath: '/status.json' },
-  { name: "Origin"     , https: 'neos21.tk'    , http: 'neo.s21.xrea.com'           , statusJsonPath: '/status.json' },
-  { name: 'OCI 1'      , https: 'neos21-oci.cf', http: '140.238.56.203'             , statusJsonPath: '/status.json' },
-  { name: 'OCI 2'      , https: 'neos21-oci.ml', http: '158.101.130.242'            , statusJsonPath: '/status.json' }
+  { name: "Neo's World", https: 'neos21.net'    , http: 'neos21.github.io/neos21.net', statusJsonPath: '/status.json' },
+  { name: "Origin"     , https: 'neos21.tk'     , http: 'neo.s21.xrea.com'           , statusJsonPath: '/status.json' },
+  { name: 'OCI 1'      , https: 'neos21-oci.cf' , http: '140.238.56.203'             , statusJsonPath: '/status.json' },
+  { name: 'OCI 2'      , https: 'app.neos21.net', http: '158.101.130.242'            , statusJsonPath: '/status.json' }
 ];
 
 /** 通知を行う有効期限日までの残日数 */
